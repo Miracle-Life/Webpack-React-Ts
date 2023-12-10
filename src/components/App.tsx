4f0527e7068549855b2088e5f04aee9d102d4f40
Denys <belovdenys@gmail.com>
2023-12-10 02:13:21 +0200

    Работа со стилями. css и scss. MiniCssExtractPlugin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import {useState} from 'react';
 
+import './App.scss';
+
 export const App = () => {
     const [count, setCount] = useState<number>(0);
     const increment = () => setCount(prev => prev + 1);
@@ -8,6 +10,7 @@ export const App = () => {
             <p>Count: {count}</p>
             <button onClick={increment}>
                 Increment
+                <span>+</span>
             </button>
         </div>
     );
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import type  { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 type Mode = 'development' | 'production';
@@ -12,19 +13,32 @@ interface EnvVariables {
 export default (env: EnvVariables) => {
 
     const isDevelopment = env.mode === 'development';
+    const isProduction = env.mode === 'production';
 
     const config: webpack.Configuration = {
         mode: env.mode ?? 'development',
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
         module: {
             rules: [
+                //порядок имеет значение
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
                 {
                     // ts-loader умеет работать JSX, Babel нет
                     // Если б мы использовали TypeScript, то нам бы не нужен был бы babel-loader
                     test: /\.tsx?$/,
                     use: 'ts-loader',
                     exclude: /node_modules/,
-                },
+                }
             ],
         },
         resolve: {
@@ -32,13 +46,17 @@ export default (env: EnvVariables) => {
         },
         output: {
             path: path.resolve(__dirname, 'build'),
-            filename: '[name].[contenthash].bundle.js',
+            filename: '[name].[contenthash].js',
             clean: true
         },
         devtool: isDevelopment ? 'inline-source-map' : false,
         plugins: [
             new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
             isDevelopment && new webpack.ProgressPlugin(),
+            isProduction && new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            })
         ].filter(Boolean),
         devServer: {
             static: {
